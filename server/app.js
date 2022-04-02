e24775b53c50ec870a71afe9880939579b099f33
Christian Eliopoulos <cge7497@rit.edu>
2022-04-02 01:31:36 -0400

    eslint formatting

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ mongoose.connect(dbURI, (err) => {
   }
 });
 
-const redisURL = process.env.REDISCLOUD_URL; //Finally squashed commits to remove url!
+const redisURL = process.env.REDISCLOUD_URL; // Finally squashed commits to remove url!
 
 const redisClient = redis.createClient({
   legacyMode: true,
